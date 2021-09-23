7928e9f157f3021f0f41e2c05a1f23e39efe4f7b
Matt Boston <mattbos10@icloud.com>
2021-09-22 23:58:09 -0500

    no bonus

---
+++ script.js
@@ -0,0 +1,29 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs);
+console.log(warmHugs.toLowerCase());
+console.log(warmHugs);
+console.log(warmHugs.replace('like', 'love'));
+console.log(warmHugs);
+let beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled);
+console.log(beenImpaled.slice(-18));
+console.log(beenImpaled);
+let dotDotDot = "..."
+console.log('...');
+let skullBones = "I don't have a skull" + dotDotDot + "or bones."
+console.log(skullBones);
+Math.PI;
+console.log(Math.PI);
+let randomNum = Math.random();
+console.log(randomNum);
+// returns a number from 0 up to 3
+randomNum *= 3;
+// randumNum = randomNum * 3;
+console.log(randomNum);
+// returns 0,1,2 or
+randomNum = Math.floor(randomNum)
+console.log(randomNum);
+// returns 1,2 or 6 --> adds 1 --> dice roll
+randomNum++;
+console.log(randomNum);
+// BONUS
